3e806a0d8ac30781748c8de5d9a11ac33f6bf9d4
Sourav Kumar <31382363+studentdevelops@users.noreply.github.com>
2021-09-26 00:35:13 +0530

    sidebar edits

--- components/SideBar.js
+++ components/SideBar.js
@@ -4,7 +4,7 @@ import { useState } from "react";
 import { FiMenu } from "react-icons/fi";
 import { AiOutlineCloseCircle } from "react-icons/ai";
 
-const SideBar = ({ toggle }) => {
+const SideBar = () => {
   const [toggleSideBar, setSideBar] = useState(true);
 
   const update = () => {
@@ -15,14 +15,11 @@ const SideBar = ({ toggle }) => {
         document.querySelector(".footer").style.marginLeft = "170px";
         document.querySelector(".sidebar").style.width = "200px";
         document.querySelector(".nav").style.marginLeft = "200px";
-        document.querySelector(".menu").style.alignSelf = "flex-end";
-
       } else {
         document.getElementById("main").style.marginLeft = "50px";
         document.querySelector(".footer").style.marginLeft = "10px";
         document.querySelector(".sidebar").style.width = "40px";
         document.querySelector(".nav").style.marginLeft = "40px";
-        document.querySelector(".menu").style.alignSelf = "center";
       }
     } else {
       if (toggleSideBar) {
@@ -31,10 +28,8 @@ const SideBar = ({ toggle }) => {
         document.querySelector(".sidebar").style.width = "120px";
         document.querySelector(".nav").style.marginLeft = "120px";
         document.querySelector(".nav").style.fontSize = "0.5rem";
-        document.querySelector(".menu").style.alignSelf = "flex-end";
       } else {
         document.getElementById("main").style.marginLeft = "50px";
-        document.querySelector(".menu").style.alignSelf = "center";
         document.querySelector(".footer").style.marginLeft = "40px";
         document.querySelector(".sidebar").style.width = "40px";
         document.querySelector(".nav").style.marginLeft = "40px";
@@ -44,9 +39,9 @@ const SideBar = ({ toggle }) => {
   };
 
   return (
-    <div className={"sidebar " + toggle}>
+    <div className={"sidebar"}>
       <ul>
-        <li className="menu" key={0}>
+        <li key={0}>
           {toggleSideBar ? (
             <FiMenu onClick={update} />
           ) : (
@@ -55,11 +50,13 @@ const SideBar = ({ toggle }) => {
         </li>
         {SideBarData.map((val, key) => {
           return (
-            <li key={key} className={SideBarData.length - 1 == key ? "pushDown " : ""}>
+            <li key={key}>
               <Link href={val.link}>
                 <a>
                   <div>{val.icon}</div>
-                  <div className={toggleSideBar? "hide": ""}>{val.title}</div>
+                  <div className={toggleSideBar ? "text" : "" }>
+                    {val.title}
+                  </div>
                 </a>
               </Link>
             </li>
