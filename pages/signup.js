67b61ea3421b0c93347e241809358758ee2b4596
vaikunthg <vag1921095@sicsr.ac.in>
2021-10-09 17:27:43 +0530

    signup

--- pages/signup.js
+++ pages/signup.js
@@ -1,9 +1,32 @@
 import { motion } from "framer-motion";
-const signup = () => {
+
+function Form(){
+    const signup = event => {
+        event.preventDefault()
+        
+        const res = await fetch('/api/register.js', {
+            body: JSON.stringify({
+              name: event.target.name.value
+            }),
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            method: 'POST'
+          })
+      
+        const result = await res.json()
+        // result.user => 'Ada Lovelace'
+
+    }
+
     return (
-        <motion.div initial='initial' animate='animate' exit={{opacity:0}}>
-            
-        </motion.div>
+           <motion.div initial='initial' animate='animate' exit={{opacity:0}}>
+            <form onSubmit={registerUser}>
+                <label htmlFor="name">Name</label>
+                <input id="name" type="text" autoComplete="name" required />
+                <button type="submit">Register</button>
+            </form>  
+           </motion.div>
     )
 }
 
